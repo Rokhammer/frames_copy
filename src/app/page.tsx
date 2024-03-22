587afb4149ada45a469711d5ead5ea8648c3d882
Stanislav Diner <stanislav.diner@gmail.com>
2024-03-22 17:00:12 +0300

    Minor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ export const metadata: Metadata = {
   openGraph: {
     title: '$PURR Frame',
     description: 'Check your $PURR',
-    images: [`${process.env.NEXT_PUBLIC_SITE_URL}/gold.png`],
+    images: [`${process.env.NEXT_PUBLIC_SITE_URL}/site-preview.jpg`],
   },
   other: {
     'fc:frame': 'vNext',
-    'fc:frame:image': `${process.env.NEXT_PUBLIC_SITE_URL}/gold.png`,
+    'fc:frame:image': `${process.env.NEXT_PUBLIC_SITE_URL}/site-preview.jpg`,
     'fc:frame:button:1:post_url': `${process.env.NEXT_PUBLIC_SITE_URL}/api?id=2`,
     //'fc:frame:button:1:target': `${process.env.NEXT_PUBLIC_SITE_URL}/api?id=1`,
     'fc:frame:post_url': `${process.env.NEXT_PUBLIC_SITE_URL}/api?id=3`,
